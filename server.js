759cad727c81d7a043490a8014230af3b3466756
Kai <84567584+kaidri@users.noreply.github.com>
2023-08-08 16:44:49 +0100

    Tutorial #51 - Added Environmental Variables

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './config.env'});
+
+// $env:NODE_ENV="development" - replace NODE_ENV with anything else to set environmental variables
+console.log(process.env);
 
 // CREATE A SERVER
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log('Server has started.');
