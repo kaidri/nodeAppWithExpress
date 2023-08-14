4681bbc2a0fa77e986dc8fc4890422a6281f5833
Kai <84567584+kaidri@users.noreply.github.com>
2023-08-14 13:09:49 +0100

    Tutorial #66 - Added schema and model
    Added Schema and Model to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
+const { ServerDescription } = require('mongodb');
 
 dotenv.config({path: './config.env'});
 
@@ -11,10 +12,44 @@ console.log(process.env);
 mongoose.connect(process.env.CONN_STR, {
     useNewUrlParser: true
 }).then((conn) => {
-    console.log('Database Connection Successful')
+    console.log('Database Connection Successful');
 }).catch((error) => {
-    console.log('An error has occurred')
+    console.log('An error has occurred');
 })
+
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is a required field.'],
+        unique: true
+    },
+    description: String,
+    duration: {
+        type: Number,
+        required: [true, 'Duration is a required field.']
+    },
+    ratings: {
+        type: Number,
+        default: 1.0
+    }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+const testMovie = new Movie({
+    name: "Die Hard",
+    description: "A festive movie...",
+    duration: 139,
+    rating: 4.5
+})
+
+testMovie.save()
+.then(doc => {
+    console.log(doc);
+})
+.catch(err => {
+    console.log('An error has occurred: ' + err);
+});
 
 const port = process.env.PORT || 3001;
 
